1798cb9c372bc161954798a4912ab0023e7f9e27
Ehsan-c137 <ehsanmousavi1.79@gmail.com>
2022-05-17 14:03:19 +0430

    add animation when page showUP

--- JS/index.js
+++ JS/index.js
@@ -9,3 +9,26 @@ searchDocCloseBtn.forEach(function (item) {
       item.closest(".search-doctor__segested-items").remove();
    });
 });
+
+// intersection observer
+const obsOption = {
+   root: null,
+   rootMargin: "0px",
+   threshold: 0.45,
+};
+
+const obsCallback = (entries, observer) => {
+   entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+         entry.target.classList.add("active-animation");
+         observer.unobserve(entry.target);
+      }
+   });
+};
+
+let observer = new IntersectionObserver(obsCallback, obsOption);
+
+const animateObs = document.querySelectorAll(".animate-obs");
+animateObs.forEach((el) => {
+   observer.observe(el);
+});
